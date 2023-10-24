d340c8e6ca123deab09557fcdd53c53b0142be03
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-10-24 15:12:22 -0300

    resolve req 1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,14 +1,31 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
-
   /**
    * Metodo main.
    */
   public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int activityQuantity = scanner.nextInt();
+    scanner.nextLine();
+
+    String[] activitiesNames = new String[activityQuantity];
+    int[] activitiesWeights = new int[activityQuantity];
+
+    for (int i = 0; i < activityQuantity; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      activitiesNames[i] = scanner.nextLine();
+
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      activitiesWeights[i] = scanner.nextInt();
+      scanner.nextLine();
+    }
+    scanner.close();
   }
 }
